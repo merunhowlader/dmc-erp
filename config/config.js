dfe4c1049c96c3438f61558c6b21a54c6b1ed6fd
merunhowlader <merunhowlader@gmail.com>
2022-04-27 10:50:18 +0600

    product update email update env update

--- config/config.js
+++ config/config.js
@@ -34,7 +34,7 @@ module.exports ={
   "production": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
+    "database": process.env.DB_NAME_LIVE,
     "host": process.env.DB_HOST,
     "dialect": "postgres",
     "port":process.env.DB_PORT,
